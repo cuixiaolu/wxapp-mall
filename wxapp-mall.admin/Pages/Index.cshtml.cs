8fa4dffceb45ef16080fa6dfad84d49ede81b1cb
lucio.cui <cuixiaolu98@gmail.com>
2018-08-21 14:16:56 +0800

    初次提交

---
+++ wxapp-mall.admin/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace wxapp_mall.admin.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
